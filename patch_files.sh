89661ffcc22fa20e523fba6cf0fbc7138dda1189
MacDesu <32233686+macdesu@users.noreply.github.com>
2019-10-24 11:21:28 +0200

    patch_files.sh seems finished

--- patch_files.sh
+++ patch_files.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-# NOTE: Run this in EverQuest's directory
+echo -e "\033[0;36m _____           _         _   ___   ___ ___ ___ \033[0m"
+echo -e "\033[0;36m|  _  |___ ___  |_|___ ___| |_|_  | | . | . | . |\033[0m"
+echo -e "\033[0;36m|   __|  _| . | | | -_|  _|  _|_| |_|_  |_  |_  |\033[0m"
+echo -e "\033[0;36m|__|  |_| |___|_| |___|___|_| |_____|___|___|___|\033[0m"
+echo -e "\033[0;36m              |___|\033[0m\n"
+wget http://www.project1999.com/files/P99Files50d.zip
+unzip -o P99Files50d.zip -d ~/.wine/drive_c/Program\ Files/Sony/EverQuest
+cd ~/.wine/drive_c/Program\ Files/Sony/EverQuest
+mv Resources/loadscreen.JPG Resources/loadscreen.jpg
+mv dsetup.dll DSETUP.dll
+echo -e "\n\033[0;32mDONE !\033[0m"
