fbbe47b94ce003a18d8c6c526db5ad1a088a4637
Mallone Delwaulle <32233686+malloc0x3cc@users.noreply.github.com>
2021-09-06 20:04:05 +0200

    added p99-login-middlemand to both scripts

--- launch_titanium.sh
+++ launch_titanium.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 export WINEPREFIX=$HOME/.wine/
+./p99-login-middlemand/bin/p99-login-middlemand &
+MIDDLEMAND_PID=$!
 cd ~/.wine/drive_c/Program\ Files\ \(x86\)/Sony/EverQuest/
 wine eqgame.exe patchme 2>/dev/null
+if [[ "$MIDDLEMAND_PID" != '' ]]; then
+	kill -9 $MIDDLEMAND_PID
+fi
--- patch_files.sh
+++ patch_files.sh
@@ -2,12 +2,18 @@
 GAMEPATH=~/.wine/drive_c/Program\ Files\ \(x86\)/Sony/EverQuest
 P99FILE=P99Files54.zip
 
+# Renaming files
 mv -v "${GAMEPATH}/Resources/loadscreen.JPG" "${GAMEPATH}/Resources/loadscreen.jpg"
 mv -v "${GAMEPATH}/DSETUP.dll" "${GAMEPATH}/dsetup.dll"
 
+# Patching the game for Project1999
 wget https://www.project1999.com/files/${P99FILE}
 unzip -ou ${P99FILE} -d "${GAMEPATH}"
 rm -vf ${P99FILE}
+
+# compiling p99-login-middlemand and patching the game to use it
+cd ./p99-login-middlemand && make
+echo -e "[LoginServer]\nHost=localhost:5998" > ${GAMEPATH}/eqhost.txt
 
 echo -e "\n\033[0;36m _____           _         _   ___   ___ ___ ___ \033[0m"
 echo -e "\033[0;36m|  _  |___ ___  |_|___ ___| |_|_  | | . | . | . |\033[0m"
